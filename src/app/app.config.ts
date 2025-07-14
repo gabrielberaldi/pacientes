4cfdb75fe966ecdeddb89750da551814c62983e8
Gabriel Beraldi <gabriel.beraldi@hotmail.com>
2025-07-13 21:21:31 -0400

    fix: routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -8,7 +8,7 @@ import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes), 
+    provideRouter(routes, withHashLocation()), 
     provideAnimationsAsync(),
      {
       provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,9 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
-  { path: '**', redirectTo: '' },
   { 
     path: 'auth',
     loadChildren: () => import('./modules/auth/auth.module').then((m) => m.AuthModule),
-  }
+  },
+  { path: '**', redirectTo: '' },
 ];
