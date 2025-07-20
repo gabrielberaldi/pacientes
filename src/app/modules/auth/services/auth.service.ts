6b5677e23e81f7c4845854010d8439f411d15958
Gabriel Beraldi <gabriel.beraldi@hotmail.com>
2025-07-20 18:41:26 -0400

    refactor: auth.service.ts

--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { BaseIpcService } from '../../../core/services/base-ipc.service';
 import { catchError, from, Observable } from 'rxjs';
 import { UserRegister } from '../model/user-register';
 import { CurrentUser } from '../../../core/models/current-user.model';
@@ -7,11 +6,7 @@ import { CurrentUser } from '../../../core/models/current-user.model';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService extends BaseIpcService<CurrentUser> {
-
-  constructor() {
-    super('auth');
-  }
+export class AuthService {
   
   login(user: UserRegister): Observable<CurrentUser> {
     return from(window.electron.invoke<CurrentUser>('auth:login', user))
