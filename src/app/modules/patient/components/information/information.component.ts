773d48e583abc6c96fa7e23b6d2ddbcb0fbb7818
Gabriel Beraldi <gabriel.beraldi@hotmail.com>
2025-09-22 19:11:10 -0400

    fix: adjust enums

--- src/app/modules/patient/components/information/information.component.ts
+++ src/app/modules/patient/components/information/information.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input, input } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { NbDatepickerModule, NbInputModule, NbOptionModule, NbSelectModule } from '@nebular/theme';
 import { Option } from '../../../../shared/model/option.model';
-import { SessionDay } from '../../models/session-day.enum';
 import { PhoneMaskDirective } from '../../../../shared/directive/phone-mask.directive';
+import { SessionDay } from '../../../../shared/enum/session-day.enum';
+import { StreetType } from '../../../../shared/enum/street-type.enum';
 
 @Component({
   selector: 'app-information',
@@ -15,21 +16,12 @@ import { PhoneMaskDirective } from '../../../../shared/directive/phone-mask.dire
 export class InformationComponent {
 
   formGroup = input.required<FormGroup>();
-
-  weekDays: Option<SessionDay>[] = [ 
-    { value: SessionDay.MONDAY, label: 'Segunda-Feira' },
-    { value: SessionDay.TUESDAY, label: 'Terça-Feira' },
-    { value: SessionDay.WEDNESDAY, label: 'Quarta-Feira' },
-    { value: SessionDay.THURSDAY, label: 'Quinta-Feira' },
-    { value: SessionDay.FRIDAY, label: 'Sexta-Feira' },
-    { value: SessionDay.SATURDAY, label: 'Sábado' },
-    { value: SessionDay.SUNDAY, label: 'Domingo' }
-  ];
-
   genderOptions: Option<string>[] = [
     { value: 'M', label: 'Masculino' },
     { value: 'F', label: 'Feminino' },
-  ]
+  ];
+  streetTypes: Option<StreetType>[] = Object.values(StreetType).map(value => ({ label: value, value }));
+  weekDays: Option<SessionDay>[] = Object.values(SessionDay).map(value => ({ label: value, value }));
 
   getControl(name: string): FormControl {
     return this.formGroup().get(name) as FormControl;
---
+++ src/app/shared/enum/session-day.enum.ts
@@ -0,0 +1,9 @@
+export enum SessionDay { 
+  MONDAY = 'Segunda-Feira',
+  TUESDAY = 'Terça-Feira',
+  WEDNESDAY = 'Quarta-Feira',
+  THURSDAY = 'Quinta-Feira',
+  FRIDAY = 'Sexta-Feira',
+  SATURDAY = 'Sábado',
+  SUNDAY = 'Domingo'
+}
